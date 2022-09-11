05f1fe2ce571d854cddbe1809953eb930548c6fc
Sam Texas <sam@simplecto.com>
2022-09-11 02:01:39 +0200

    DA-17 [github_api] should use pickle serialization when talking to redis

---
+++ tests/test_cache_adapters.py
@@ -0,0 +1,15 @@
+import unittest
+from github_api_crawler.cache_adaptors import RedisCacheAdapter
+
+
+class TestGithub_cache_adapters(unittest.TestCase):
+    """Tests for `github_api_crawler` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
